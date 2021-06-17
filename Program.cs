d7a7e14c716c42d711f3d451b6bd6f4f5f4a16c0
Comelli <ivansrib@gmail.com>
2021-06-17 16:58:33 -0300

    soma valores

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exerc5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int soma = 0;
+
+
+            Console.WriteLine("Digite um valor numerico");
+            string valor = Console.ReadLine();
+
+
+            int[] numero = new int[valor.Length];
+            int tamanho = numero.Length;
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                 
+                String a = valor.Substring(i,1);
+                int aux = Int32.Parse(a);
+                numero[i] = aux;
+                soma = aux+soma;
+            }
+
+            
+          
+            Console.WriteLine("Soma do numero" + soma);
+        }
+    }
+}
